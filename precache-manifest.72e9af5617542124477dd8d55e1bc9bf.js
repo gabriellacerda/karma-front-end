0e77c64e1171176263d030e34f86f66e43d5f846
GabrielLacerda <gabriellacerdadesign@gmail.com>
2020-04-07 16:48:03 -0300

    Updates

---
+++ precache-manifest.72e9af5617542124477dd8d55e1bc9bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836aef03ce1ad417d14821eab2b41721",
+    "url": "/karma-front-end/index.html"
+  },
+  {
+    "revision": "a011315e3440bafe7513",
+    "url": "/karma-front-end/static/css/main.9137fb8f.chunk.css"
+  },
+  {
+    "revision": "3b524ac3b7ba1f389755",
+    "url": "/karma-front-end/static/js/2.617cc598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karma-front-end/static/js/2.617cc598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a011315e3440bafe7513",
+    "url": "/karma-front-end/static/js/main.d1bc88c6.chunk.js"
+  },
+  {
+    "revision": "6d8e097ccf29545f3fc2",
+    "url": "/karma-front-end/static/js/runtime-main.65478dea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karma-front-end/static/media/logo.5d5d9eef.svg"
+  }
+]);
